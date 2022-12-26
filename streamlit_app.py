6a4a60e109b342bc070d2e458b56b5e05415e38f
oldmonk gotya <120905769+oldmonkgotya@users.noreply.github.com>
2022-12-26 21:01:58 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ streamlit.text('avcado tost')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some furits:",list(my_fruit_list.index),['Avocado','straberries'])
+streamlit.multiselect("Pick some furits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
